a4646865f18ff0a5ebb2ec4cfc283747a854a528
clubjk <jk@jkcybersecurity.com>
2023-07-28 16:26:54 -0500

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import data from "./data";
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>;
+      })}
+    </section>
+  );
+};
+export default App;
